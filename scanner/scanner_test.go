e14cf71796a7565f2bfb324fc48e8de860e81a70
Shunsuke Michii <harukasan@me.com>
2016-08-29 13:16:40 +0900

    Make convenient method to test scaninng token.

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -8,6 +8,12 @@ import (
 	"github.com/harukasan/ringo/token"
 )
 
+func assertScanToken(offset int, token token.Token, literal []byte) func(t *testing.T, s *Scanner) {
+	return func(t *testing.T, s *Scanner) {
+		assertScan(t, s, offset, token, literal)
+	}
+}
+
 func assertScan(t *testing.T, s *Scanner, pos int, token token.Token, literal []byte) bool {
 	gp, gt, gl := s.Scan()
 	if gp != pos || gt != token || !bytes.Equal(gl, literal) {
@@ -20,220 +26,90 @@ func assertScan(t *testing.T, s *Scanner, pos int, token token.Token, literal []
 
 var rules = map[string]func(t *testing.T, s *Scanner){
 	// new line
-	"\n": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.NewLine, nil)
-	},
-	"\r\n": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 1, token.NewLine, nil)
-	},
+	"\n":   assertScanToken(0, token.NewLine, nil),
+	"\r\n": assertScanToken(1, token.NewLine, nil),
+
 	// white spaces
-	"": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.EOF, nil)
-	},
-	" ": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 1, token.EOF, nil)
-	},
-	" \t": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 2, token.EOF, nil)
-	},
+	"":    assertScanToken(0, token.EOF, nil),
+	" ":   assertScanToken(1, token.EOF, nil),
+	" \t": assertScanToken(2, token.EOF, nil),
+
 	// comment
-	" #": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 2, token.EOF, nil)
-	},
+	" #": assertScanToken(2, token.EOF, nil),
 	"a#b": func(t *testing.T, s *Scanner) {
 		assertScan(t, s, 0, token.IDENT, []byte("a"))
 		assertScan(t, s, 3, token.EOF, nil)
 	},
+
 	// operators
-	"!": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Not, nil)
-	},
-	"!=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.NotEqual, nil)
-	},
-	"!~": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.NotMatch, nil)
-	},
-	"&&": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AndOperator, nil)
-	},
-	"||": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.OrOperator, nil)
-	},
+	"!":  assertScanToken(0, token.Not, nil),
+	"!=": assertScanToken(0, token.NotEqual, nil),
+	"!~": assertScanToken(0, token.NotMatch, nil),
+	"&&": assertScanToken(0, token.AndOperator, nil),
+	"||": assertScanToken(0, token.OrOperator, nil),
+
 	// operator methods
-	"^": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Xor, nil)
-	},
-	"&": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Amp, nil)
-	},
-	"|": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Or, nil)
-	},
-	"<=>": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Compare, nil)
-	},
-	"==": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Eq, nil)
-	},
-	"===": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Eql, nil)
-	},
-	"=~": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Match, nil)
-	},
-	">": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Gt, nil)
-	},
-	">=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.GtEq, nil)
-	},
-	"<": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Lt, nil)
-	},
-	"<=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.LtEq, nil)
-	},
-	"<<": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.LShift, nil)
-	},
-	">>": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.RShift, nil)
-	},
-	"+": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Plus, nil)
-	},
-	"-": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Minus, nil)
-	},
-	"*": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Mul, nil)
-	},
-	"/": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Div, nil)
-	},
-	"%": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Mod, nil)
-	},
-	"**": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Pow, nil)
-	},
-	"~": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Invert, nil)
-	},
-	"+@": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.UnaryPlus, nil)
-	},
-	"-@": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.UnaryMinus, nil)
-	},
-	"[]": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.ElementRef, nil)
-	},
-	"[]=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.ElementSet, nil)
-	},
+	"^":   assertScanToken(0, token.Xor, nil),
+	"&":   assertScanToken(0, token.Amp, nil),
+	"|":   assertScanToken(0, token.Or, nil),
+	"<=>": assertScanToken(0, token.Compare, nil),
+	"==":  assertScanToken(0, token.Eq, nil),
+	"===": assertScanToken(0, token.Eql, nil),
+	"=~":  assertScanToken(0, token.Match, nil),
+	">":   assertScanToken(0, token.Gt, nil),
+	">=":  assertScanToken(0, token.GtEq, nil),
+	"<":   assertScanToken(0, token.Lt, nil),
+	"<=":  assertScanToken(0, token.LtEq, nil),
+	"<<":  assertScanToken(0, token.LShift, nil),
+	">>":  assertScanToken(0, token.RShift, nil),
+	"+":   assertScanToken(0, token.Plus, nil),
+	"-":   assertScanToken(0, token.Minus, nil),
+	"*":   assertScanToken(0, token.Mul, nil),
+	"/":   assertScanToken(0, token.Div, nil),
+	"%":   assertScanToken(0, token.Mod, nil),
+	"**":  assertScanToken(0, token.Pow, nil),
+	"~":   assertScanToken(0, token.Invert, nil),
+	"+@":  assertScanToken(0, token.UnaryPlus, nil),
+	"-@":  assertScanToken(0, token.UnaryMinus, nil),
+	"[]":  assertScanToken(0, token.ElementRef, nil),
+	"[]=": assertScanToken(0, token.ElementSet, nil),
+
 	// assign operators
-	"=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Assign, nil)
-	},
-	"&&=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignAndOperator, nil)
-	},
-	"||=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignOrOperator, nil)
-	},
-	"^=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignXor, nil)
-	},
-	"&=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignAnd, nil)
-	},
-	"|=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignOr, nil)
-	},
-	"<<=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignLShift, nil)
-	},
-	">>=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignRShift, nil)
-	},
-	"+=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignPlus, nil)
-	},
-	"-=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignMinus, nil)
-	},
-	"*=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignMul, nil)
-	},
-	"/=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignDiv, nil)
-	},
-	"%=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignMod, nil)
-	},
-	"**=": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.AssignPow, nil)
-	},
+	"=":   assertScanToken(0, token.Assign, nil),
+	"&&=": assertScanToken(0, token.AssignAndOperator, nil),
+	"||=": assertScanToken(0, token.AssignOrOperator, nil),
+	"^=":  assertScanToken(0, token.AssignXor, nil),
+	"&=":  assertScanToken(0, token.AssignAnd, nil),
+	"|=":  assertScanToken(0, token.AssignOr, nil),
+	"<<=": assertScanToken(0, token.AssignLShift, nil),
+	">>=": assertScanToken(0, token.AssignRShift, nil),
+	"+=":  assertScanToken(0, token.AssignPlus, nil),
+	"-=":  assertScanToken(0, token.AssignMinus, nil),
+	"*=":  assertScanToken(0, token.AssignMul, nil),
+	"/=":  assertScanToken(0, token.AssignDiv, nil),
+	"%=":  assertScanToken(0, token.AssignMod, nil),
+	"**=": assertScanToken(0, token.AssignPow, nil),
+
 	// numeric literals
-	"+1": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.DecimalInteger, []byte("+1"))
-	},
-	"-1": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.DecimalInteger, []byte("-1"))
-	},
-	"0": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.DecimalInteger, []byte("0"))
-	},
-	"123_456_789_0": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.DecimalInteger, []byte("123_456_789_0"))
-	},
-	"0d1234567890": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.DecimalInteger, []byte("0d1234567890"))
-	},
-	"0b0101": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.BinaryInteger, []byte("0b0101"))
-	},
-	"0B0101": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.BinaryInteger, []byte("0B0101"))
-	},
-	"0_12345670": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.OctadecimalInteger, []byte("0_12345670"))
-	},
-	"0o12345670": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.OctadecimalInteger, []byte("0o12345670"))
-	},
-	"0O12345670": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.OctadecimalInteger, []byte("0O12345670"))
-	},
-	"0x1234567890abcdef": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.HexadecimalInteger, []byte("0x1234567890abcdef"))
-	},
-	"0X1234567890abcdef": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.HexadecimalInteger, []byte("0X1234567890abcdef"))
-	},
-	"+0x1": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.HexadecimalInteger, []byte("+0x1"))
-	},
-	"-0X1": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.HexadecimalInteger, []byte("-0X1"))
-	},
-	"0.1": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Float, []byte("0.1"))
-	},
-	"+0.1": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Float, []byte("+0.1"))
-	},
-	"-0.1": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Float, []byte("-0.1"))
-	},
-	"123.0456789e10": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Float, []byte("123.0456789e10"))
-	},
-	"123.0456789E10": func(t *testing.T, s *Scanner) {
-		assertScan(t, s, 0, token.Float, []byte("123.0456789E10"))
-	},
+	"+1":                 assertScanToken(0, token.DecimalInteger, []byte("+1")),
+	"-1":                 assertScanToken(0, token.DecimalInteger, []byte("-1")),
+	"0":                  assertScanToken(0, token.DecimalInteger, []byte("0")),
+	"123_456_789_0":      assertScanToken(0, token.DecimalInteger, []byte("123_456_789_0")),
+	"0d1234567890":       assertScanToken(0, token.DecimalInteger, []byte("0d1234567890")),
+	"0b0101":             assertScanToken(0, token.BinaryInteger, []byte("0b0101")),
+	"0B0101":             assertScanToken(0, token.BinaryInteger, []byte("0B0101")),
+	"0_12345670":         assertScanToken(0, token.OctadecimalInteger, []byte("0_12345670")),
+	"0o12345670":         assertScanToken(0, token.OctadecimalInteger, []byte("0o12345670")),
+	"0O12345670":         assertScanToken(0, token.OctadecimalInteger, []byte("0O12345670")),
+	"0x1234567890abcdef": assertScanToken(0, token.HexadecimalInteger, []byte("0x1234567890abcdef")),
+	"0X1234567890abcdef": assertScanToken(0, token.HexadecimalInteger, []byte("0X1234567890abcdef")),
+	"+0x1":               assertScanToken(0, token.HexadecimalInteger, []byte("+0x1")),
+	"-0X1":               assertScanToken(0, token.HexadecimalInteger, []byte("-0X1")),
+	"0.1":                assertScanToken(0, token.Float, []byte("0.1")),
+	"+0.1":               assertScanToken(0, token.Float, []byte("+0.1")),
+	"-0.1":               assertScanToken(0, token.Float, []byte("-0.1")),
+	"123.0456789e10":     assertScanToken(0, token.Float, []byte("123.0456789e10")),
+	"123.0456789E10":     assertScanToken(0, token.Float, []byte("123.0456789E10")),
 	"x+1": func(t *testing.T, s *Scanner) {
 		assertScan(t, s, 0, token.IDENT, []byte("x"))
 		assertScan(t, s, 1, token.Plus, nil)
@@ -252,6 +128,9 @@ var rules = map[string]func(t *testing.T, s *Scanner){
 		assertScan(t, s, 0, token.IDENT, []byte("x"))
 		assertScan(t, s, 2, token.DecimalInteger, []byte("-1"))
 	},
+
+	// string literals:
+
 	// ident
 	"a": func(t *testing.T, s *Scanner) {
 		assertScan(t, s, 0, token.IDENT, []byte("a"))
